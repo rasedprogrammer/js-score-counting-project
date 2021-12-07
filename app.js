8df33306c8ca910f89f05cee53cb0fc91f64317d
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-08 00:36:48 +0600

    done with reset button logics in

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const winningScoreSelect = document.querySelector("#playto");
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 p1Button.addEventListener("click", function () {
   if (!isGameOver) {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
     }
     p1Display.textContent = p1Score;
   }
@@ -23,9 +25,11 @@ p2Button.addEventListener("click", function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
     }
-    p2Display.textContent = p2Score;
   }
+  p2Display.textContent = p2Score;
 });
 
 winningScoreSelect.addEventListener("change", function () {
@@ -40,6 +44,8 @@ function reset() {
   p2Display.textContent = 0;
   p1Score = 0;
   p1Score = 0;
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("loser", "loser");
 }
 
 // p2Button.addEventListener("click", function () {
